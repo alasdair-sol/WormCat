e2230066eefbb4ba3327a2e7687182ed5830cab3
alasdairhendry <alasdair@sol.us>
2024-03-07 23:19:38 +0000

    Add Data Layer Class Library and initialise efcore dbcontext

---
+++ WormCat.Data/Data/WormCatRazorContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WormCat.Library.Models;
+
+namespace WormCat.Data.Data
+{
+    public class WormCatRazorContext : DbContext
+    {
+        public WormCatRazorContext(DbContextOptions<WormCatRazorContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Record> Record { get; set; } = default!;
+        public DbSet<Book> Book { get; set; } = default!;
+        public DbSet<Container> Container { get; set; } = default!;
+        public DbSet<Location> Location { get; set; } = default!;
+    }
+}
---
+++ WormCat.Data/Migrations/20240228130707_InitialTables.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WormCat.Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Location",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StreetNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StreetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Container",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Container", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Container_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Location",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    ContainerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Book_Container_ContainerId",
+                        column: x => x.ContainerId,
+                        principalTable: "Container",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_ContainerId",
+                table: "Book",
+                column: "ContainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Container_LocationId",
+                table: "Container",
+                column: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Container");
+
+            migrationBuilder.DropTable(
+                name: "Location");
+        }
+    }
+}
