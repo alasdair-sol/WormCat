ba94624d82c89c243b1c5d89add1c774163c4749
alasdairhendry <alasdair@sol.us>
2024-03-11 19:30:16 +0000

    Add Search endpoint & move search bar to shared layout

--- WormCat.Razor/Pages/Search/Index.cshtml.cs
+++ WormCat.Razor/Pages/Search/Index.cshtml.cs
@@ -5,33 +5,28 @@ using Microsoft.EntityFrameworkCore;
 using WormCat.Library.Models;
 using WormCat.Razor.Models;
 
-namespace WormCat.Razor.Pages
+namespace WormCat.Razor.Pages.Search
 {
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly WormCat.Data.Data.WormCatRazorContext _context;
-        private readonly SeedDatabase _seedDatabase;
+
+        public IList<Record> Records { get; set; } = new List<Record>();
 
         [BindProperty(SupportsGet = true)]
         public string? query { get; set; } = string.Empty;
-
-        [BindProperty(SupportsGet = true)]
-        public string? action { get; set; } = string.Empty;
 
         public IndexModel(ILogger<IndexModel> logger, WormCat.Data.Data.WormCatRazorContext context, SeedDatabase seedDatabase)
         {
             _logger = logger;
             _context = context;
-            this._seedDatabase = seedDatabase;
         }
-
-        public IList<Record> Records { get; set; } = new List<Record>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            _logger.LogInformation("OnGetAsync");
+            _logger.LogInformation("Search.OnGetAsync");
 
             Records = await _context.Record.Include(x => x.Books).ThenInclude(x => x.Container).ThenInclude(x => x.Location).ToListAsync();
 
@@ -57,21 +52,6 @@ namespace WormCat.Razor.Pages
             }
 
             return Page();
-        }
-
-        public IActionResult OnGetSeedDatabase()
-        {
-            _logger.LogInformation("Seed");
-            _seedDatabase.TrySeed(true);
-
-            return LocalRedirect("/?action=1");
-        }
-
-        public async Task Clear()
-        {
-            _logger.LogInformation("Clear");
-            _seedDatabase.Clear();
-            await OnGetAsync();
         }
     }
 }
